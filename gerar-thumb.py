1a864e02ce690dcd5731e4468188468556dde1fb
Francisco Matelli <tercmt-francisco@ftd.com.br>
2023-07-09 00:26:46 -0300

    finaliza rotina de geracao de imagem na pasta de anuncios, com prefixo _anuncio-, colocando no topo e permitindo varios precos de anuncios diferentes

--- gerar-thumb.py
+++ gerar-thumb.py
@@ -13,8 +13,9 @@ from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPDF, renderPM
 from cairosvg import svg2pdf
 from distutils.dir_util import copy_tree
+import shutil
 
-current_path = os.path.dirname(os.path.realpath(__file__)) + "/"
+path_execucao_script = os.path.dirname(os.path.realpath(__file__)) + "/"
 
 ## SETTINGS MANAGER ##
 
@@ -24,7 +25,15 @@ path_anuncios = "/Users/francisco/Downloads/anuncios-controle/usados/"
 #modelo do anuncio atual
 modelo_anuncio = "lojasdomago-instagram"
 modelo_os = "-macos"
-current_path_modelo_anuncio = current_path+'modelos-de-anuncios/' + modelo_anuncio + '/'
+path_execucao_script_modelo_anuncio = path_execucao_script+'modelos-de-anuncios/' + modelo_anuncio + '/'
+path_execucao_script_anuncios_prontos = path_execucao_script + "anuncios-prontos/" + modelo_anuncio + "/"
+
+#Precisa remover tudo, porque é gerado com Nome  e preço no final, e quando vender tem que sair
+if os.path.isdir(path_execucao_script_anuncios_prontos):
+    shutil.rmtree(path_execucao_script_anuncios_prontos)
+
+#Depois de apagar, recria
+os.mkdir(path_execucao_script_anuncios_prontos) 
 
 #Dropbox folder
 path_dropbox = "/Users/francisco/Dropbox/"
@@ -36,12 +45,12 @@ def geraThumbSVG(novo_preco, novo_titulo, path_scaneado, file_relative_path, fil
     search_titulo = "TROCARTITULO"
     search_imagem = "TROCARIMAGEM"
     
-    svg_gerado = current_path_modelo_anuncio + "svgs/" + novo_titulo + ".svg"
+    svg_gerado = path_execucao_script_modelo_anuncio + "svgs/" + novo_titulo + ".svg"
     novo_imagem_bg = path_scaneado + file_relative_path + "/" + filename
     print(" \n geraThumbSVG(), svg_gerado: " + svg_gerado + " \n novo_imagem_bg: " + novo_imagem_bg)
     novo_preco = novo_preco.replace("$", "").replace("R", "")
     ###PEGAR TAMANHO DA IMAGEM E ESCOLHER MODELO CERTO?
-    with open(current_path_modelo_anuncio + modelo_anuncio + modelo_os + '.svg', 'rt') as file:
+    with open(path_execucao_script_modelo_anuncio + modelo_anuncio + modelo_os + '.svg', 'rt') as file:
         data = file.read()
         data = data.replace(search_titulo, novo_titulo)
         data = data.replace(search_preco, novo_preco)
@@ -51,9 +60,12 @@ def geraThumbSVG(novo_preco, novo_titulo, path_scaneado, file_relative_path, fil
     with open(svg_gerado, 'w', encoding='utf-8') as file:
         file.write(data)
 
-    salvar_imagem_na_pasta_de_anuncio = path_scaneado + file_relative_path + "/" + novo_preco + "-" + novo_titulo + ".jpg"
-    salvar_imagem_na_pasta_do_script = current_path + "anuncios-prontos/" + modelo_anuncio + "/" + novo_titulo + ".jpg"
+    #
+    salvar_imagem_na_pasta_de_anuncio = path_scaneado + file_relative_path + "/_anuncio-" + novo_preco + "-" + novo_titulo + ".jpg"
+    salvar_imagem_na_pasta_do_script = path_execucao_script + "anuncios-prontos/" + modelo_anuncio + "/" + novo_titulo + ".jpg"
+    #
     drawing = svg2rlg(svg_gerado)
+    #
     renderPM.drawToFile(drawing, salvar_imagem_na_pasta_de_anuncio, fmt="JPG")
     renderPM.drawToFile(drawing, salvar_imagem_na_pasta_do_script, fmt="JPG")
     print (" \n salvar_imagem_na_pasta_de_anuncio: " + salvar_imagem_na_pasta_de_anuncio)
@@ -69,13 +81,15 @@ def scanDir(path_scaneado):
         if os.path.isdir(dirpath) and dirpath != "." and dirpath != "svgs":
             index = 1;
             print("Array de arquivos indexas no diretório: ")
-            print(sorted(filenames))
-            for filename in sorted(filenames):
-                if(filename[0:1] == "1-"):
-                    print("REMOVENDO ARQUIVO OBSOLETO ENCONTRADO")
-                    os.remove(path_scaneado+dirpath+"/"+filename)
+            print(sorted(filenames, reverse=True))
+            for filename in sorted(filenames, reverse=True):
+                if(  filename[0:9] == "_anuncio-" ):
+                    print("Encontrado thumbnail renderizada (já com preço), buscando próxima imagem para geraThumbSVG()")
+                    #os.remove(path_scaneado+dirpath+"/"+filename)
                 else:
-                    if(filename != ".DS_Store"):
+                    if( filename != ".DS_Store" and 
+                        filename.lower().endswith(('.jpg')) and not 
+                        filename.lower().endswith(('lowquality.jpg')) ):
                         if(index == 1):
                             file_preco = dirpath[-4:]
                             file_titulo =  os.path.basename(dirpath[2:])
@@ -89,8 +103,12 @@ def scanDir(path_scaneado):
     copyFolderToDropbox()
 
 def copyFolderToDropbox():
-    arquivos_na_pasta_do_script = current_path + "anuncios-prontos/" + modelo_anuncio + "/"
-    copy_tree(arquivos_na_pasta_do_script, path_dropbox + "anucios-prontos/" + modelo_anuncio)
-copyFolderToDropbox()
-#scanDir(path_anuncios)
+    path_dropbox_anuncios_prontos = path_dropbox + "anucios-prontos/" + modelo_anuncio
+    #
+    if os.path.isdir(path_dropbox_anuncios_prontos):
+        shutil.rmtree(path_dropbox_anuncios_prontos)
+    #
+    copy_tree(path_execucao_script_anuncios_prontos, path_dropbox_anuncios_prontos)
+
+scanDir(path_anuncios)
 
